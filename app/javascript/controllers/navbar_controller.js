0035606c4356b3810c88d56f40777f868a854a25
Carla Valdivia <valdivia.carla@gmail.com>
2022-12-22 00:28:20 -0300

    ADD dropdown menu

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="navbar"
+export default class extends Controller {
+  static targets = ['avatar', 'menu']
+  connect() {
+    // console.log(this.avatarTarget);
+    // console.log(this.menuTarget);
+  }
+  dropdown() {
+    this.menuTarget.classList.toggle("hidden");
+  }
+}
