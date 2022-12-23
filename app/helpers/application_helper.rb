1131479e4894c2bbf80fd9650ee372153812df0c
Carla Valdivia <valdivia.carla@gmail.com>
2022-12-23 19:41:12 -0300

    ADD sign in design LG

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def render_navbar_conditionally
+    unless current_page?(new_user_session_path) || current_page?(new_user_registration_path)
+      render 'shared/navbar'
+    end
+  end
 end
