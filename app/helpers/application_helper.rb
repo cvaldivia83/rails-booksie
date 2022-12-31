8096f7b4b90d429d5009e6fb3695c8432d6ca6a8
Carla Valdivia <valdivia.carla@gmail.com>
2022-12-30 21:16:39 -0300

    FIX: helper navbar conditionally

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,13 @@
 module ApplicationHelper
   def render_navbar_conditionally
-    unless current_page?(new_user_session_path) || current_page?(new_user_registration_path)
+     unless current_page?(new_user_session_path) || current_page?(new_user_registration_path)
       render 'shared/navbar'
+    end
+
+    if current_page?(root_path)
+      render 'shared/navbar'
+    else
+      render 'shared/navbar-bg'
     end
   end
 end
