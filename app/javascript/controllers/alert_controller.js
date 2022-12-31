d68868cafabd6f65951f751487c0842d706bf512
Carla Valdivia <valdivia.carla@gmail.com>
2022-12-30 21:39:47 -0300

    FEAT: add js to alert messages

--- app/javascript/controllers/alert_controller.js
+++ app/javascript/controllers/alert_controller.js
@@ -2,7 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="alert"
 export default class extends Controller {
+  static targets = ['alert', 'notice']
   connect() {
-    console.log("alert is working!");
+    setTimeout(() => {
+      this.alertTarget.style.display = "none";
+    }, 600);
+
+    setTimeout(() => {
+      this.noticeTarget.style.display = "none";
+    }, 600);
+  }
+
+  closeAlert(event) {
+    console.log(event.currentTarget);
+    event.currentTarget.style.opacity = '0';
   }
 }
