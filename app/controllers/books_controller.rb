e7aa9be54ed7aed0c548f494177ce0126a8d48d2
Carla Valdivia <valdivia.carla@gmail.com>
2023-03-29 20:56:16 -0300

    ADD book-show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,16 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: %i[show]
+
   def index
     @books = policy_scope(Book)
+  end
+
+  def show
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
   end
 end
