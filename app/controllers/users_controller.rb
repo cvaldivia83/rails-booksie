650f64d13c7747c3bba0bc2a157b8981274b23c5
Carla Valdivia <valdivia.carla@gmail.com>
2023-03-29 21:14:39 -0300

    ADD pundit - user show & super_booksie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
-
   def show
     @user = User.find(params[:id])
+    authorize @user
   end
 
   def super_booksie
     @booksies = User.where(super_booksie: true)
+    authorize @booksies
   end
 end
