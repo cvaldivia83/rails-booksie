38e707d46f2e8ade1f1aad1573e0cd678a3c9805
Carla Valdivia <valdivia.carla@gmail.com>
2023-03-29 22:02:25 -0300

    ADD title & subtitle + tailwind classes

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -2,24 +2,25 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
     extend: {
       grayscale: {
-        50: '50%',
+        50: "50%",
       },
       fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
         pacifico: ["Pacifico", "sans-serif"],
-      }
+      },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-  ]
-}
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+  ],
+};
