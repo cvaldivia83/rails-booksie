86fcea29dffb1a4ba63395d2e227c3ca97fe94e2
Carla Valdivia <valdivia.carla@gmail.com>
2023-03-29 23:44:33 -0300

    ADD super booksie stimulus controller

---
+++ app/javascript/controllers/super_booksie_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+import Swiper, { Navigation, Pagination } from "swiper";
+
+
+// Connects to data-controller="super-booksie"
+export default class extends Controller {
+  connect() {
+    // console.log('Super Booksie is working!');
+  }
+}
