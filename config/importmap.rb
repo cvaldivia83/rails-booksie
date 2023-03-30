f16d4c13db51a6490138171fbde580d7aef52b78
Carla Valdivia <valdivia.carla@gmail.com>
2023-03-29 23:33:26 -0300

    installed swiper.js

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "typed.js", to: "https://ga.jspm.io/npm:typed.js@2.0.14/dist/typed.module.js"
+pin "swiper", to: "https://ga.jspm.io/npm:swiper@9.1.1/swiper.esm.js"
+pin "ssr-window", to: "https://ga.jspm.io/npm:ssr-window@4.0.2/ssr-window.esm.js"
