e6efd6dc7e27386f775b948e7a089fd8c90fbdfe
Carla Valdivia <valdivia.carla@gmail.com>
2023-03-31 16:24:26 -0300

    REM authentication permission

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[show]
+  skip_before_action :authenticate_user!, only: %i[index show]
   def index
     @posts = policy_scope(Post)
   end
