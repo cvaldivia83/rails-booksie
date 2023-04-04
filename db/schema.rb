7287a7ab68cbe42deca2605a24563ef09c154ca7
Carla Valdivia <valdivia.carla@gmail.com>
2023-04-04 13:02:07 -0300

    ADD rating to books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_29_203727) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_04_155621) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_29_203727) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rating"
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
