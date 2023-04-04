90b0d37099246fef4e83d94d48b70a62ed574187
Carla Valdivia <valdivia.carla@gmail.com>
2023-04-04 11:24:50 -0300

    FIX navbar conditionally method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     if current_page?(root_path)
       render 'shared/navbar'
     elsif current_page?(new_user_session_path) || current_page?(new_user_registration_path)
-
+      render :nothing => true
     else
       render 'shared/navbar_bckgrd'
     end
