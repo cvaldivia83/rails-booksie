787777a811b7da79f8a6b356109ea8fd4dfe8e44
Carla Valdivia <valdivia.carla@gmail.com>
2023-04-03 22:31:47 -0300

    FIX super_booksie index

--- app/javascript/controllers/super_booksie_controller.js
+++ app/javascript/controllers/super_booksie_controller.js
@@ -5,7 +5,6 @@ import Swiper, { Navigation, Pagination } from "swiper";
 // Connects to data-controller="super-booksie"
 export default class extends Controller {
   connect() {
-    console.log(Pagination);
     var optionals = {
       modules: [Navigation, Pagination],
       direction: "horizontal",
