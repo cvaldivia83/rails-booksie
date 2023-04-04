16f2b6a7afff7d4cd61ab9118dbed8a7be6fb18e
Carla Valdivia <valdivia.carla@gmail.com>
2023-04-03 22:31:17 -0300

    ADD devise helper for resource

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,18 @@ module ApplicationHelper
       render 'shared/navbar_bckgrd'
     end
   end
+
+# make devise resource available for partials
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
