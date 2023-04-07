1d28640a348285eb53da84953ca3259a96660eed
Carla Valdivia <valdivia.carla@gmail.com>
2023-04-07 18:40:10 -0300

    ADD pg_search gem + model method

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,11 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
+
+  include PgSearch::Model
+  pg_search_scope :search_in_books,
+    against: %i[ title description year author publisher isbn ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
