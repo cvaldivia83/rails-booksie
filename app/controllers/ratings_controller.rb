8bf42df89d4f00939d2872eea89410303fc2efc6
Carla Valdivia <valdivia.carla@gmail.com>
2023-04-08 13:28:31 -0300

    ADD rating policy

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,8 +1,17 @@
 class RatingsController < ApplicationController
+  before_action :set_book, only: [:create]
   def create
+    @rating = Rating.new(rating_params)
+    @rating.book = @book
+    authorize @rating
+    @rating.save
   end
 
   private
+
+  def set_book
+    @book = Book.find(params[:book_id])
+  end
 
   def rating_params
     params.require(:rating).permit(:rating)
