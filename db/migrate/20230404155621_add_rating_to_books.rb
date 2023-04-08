a051f1710adf9cdfee546dca9f04a4e60d0e27a5
Carla Valdivia <valdivia.carla@gmail.com>
2023-04-08 13:14:02 -0300

    ADD ratings table

--- db/migrate/20230404155621_add_rating_to_books.rb
+++
@@ -1,5 +0,0 @@
-class AddRatingToBooks < ActiveRecord::Migration[7.0]
-  def change
-    add_column :books, :rating, :decimal, precision: 2, scale: 1
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_04_155621) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_08_160901) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,7 +52,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_04_155621) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.decimal "rating", precision: 2, scale: 1
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
@@ -76,6 +75,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_04_155621) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "ratings", force: :cascade do |t|
+    t.integer "rating"
+    t.bigint "book_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_ratings_on_book_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -97,4 +104,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_04_155621) do
   add_foreign_key "books", "users"
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "ratings", "books"
 end
