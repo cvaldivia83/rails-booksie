b70410868111cadba9a131c3afd72a8e161d33c7
Carla Valdivia <valdivia.carla@gmail.com>
2023-06-24 05:55:06 -0300

    pundit & booking new action & view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,4 +3,10 @@ class BookingsController < ApplicationController
   def index
     @bookings = Booking.all
   end
+
+  def new
+    @book = Book.find(params[:book_id])
+    @booking = Booking.new
+    authorize @booking
+  end
 end
