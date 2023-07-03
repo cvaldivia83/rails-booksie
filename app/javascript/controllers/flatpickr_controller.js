d9d1192dfd7993cdeefd6750f5561f33f2f63844
Carla Valdivia <valdivia.carla@gmail.com>
2023-07-03 13:10:15 -0300

    booking datepicker with flatpickr

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,11 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr";
 
+
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
   static targets = [ "start", "end" ]
   connect() {
-    flatpickr(this.startTarget, {})
-    flatpickr(this.endTarget, {})
+    flatpickr(this.startTarget, {
+      altInput: true });
+    flatpickr(this.endTarget, { altInput: true });
   }
 }
