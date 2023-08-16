477ee12a275a4defac8547832011ea6a2a7ca802
Carla Valdivia <valdivia.carla@gmail.com>
2023-08-15 23:36:52 -0300

    FRIENDSHIPS index action"

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,12 @@
 class FriendshipsController < ApplicationController
+  def index
+    @friendships = policy_scope(Friendship)
+  end
+
+  def create
+    @user = User.find(params[:id])
+    @friendship = Friendship.new()
+    @friendship.asker = current_user
+    @friendship.receiver = @user
+  end
 end
